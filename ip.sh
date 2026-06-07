46c488309ad39d0d5f1c0ceb23de35af2565a6c3
Dictovod <dictovod@gmail.com>
2026-06-07 06:29:28 +0200

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Interval (seconds): " SEC
+SEC=${1:-5}
 while true; do
     IP=$(curl -s --max-time 3 https://api.ipify.org 2>/dev/null || echo "unavailable")
     echo "$(date '+%Y-%m-%d %H:%M:%S') IP: $IP"
