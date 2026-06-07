6ae61df9fbe6953625c40679137f0497822211dc
Dictovod <dictovod@gmail.com>
2026-06-07 06:33:31 +0200

    Delete ip.sh

--- ip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SEC=${1:-5}
-while true; do
-    IP=$(curl -s --max-time 3 https://api.ipify.org 2>/dev/null || echo "unavailable")
-    echo "$(date '+%Y-%m-%d %H:%M:%S') IP: $IP"
-    sleep "$SEC"
-done
